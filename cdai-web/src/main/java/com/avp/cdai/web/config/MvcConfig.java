de1e34cc92c6173f5eccfab69cd47d2d52b04359
pw971959756 <pengwei940613@163.com>
2017-08-04 17:00:27 +0800

    Signed-off-by: pw971959756 <pengwei940613@163.com> 成都AFC分析系统，项目出版。--由艾文普冯达冯工提供第一版源码。

---
+++ cdai-web/src/main/java/com/avp/cdai/web/config/MvcConfig.java
@@ -0,0 +1,26 @@
+package com.avp.cdai.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Hongfei
+ */
+@Configuration
+@SessionAttributes("authorizationRequest")
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // main -- dashboard
+        registry.addViewController("/index.html").setViewName("cdai/main");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    }
+}
